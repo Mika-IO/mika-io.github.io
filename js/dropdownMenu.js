eb3b9ecd408c644dc73a3334fd07437d3d12fca6
Mikaio Faria <mikaiodev@gmail.com>
2023-05-15 20:07:32 -0400

    feat:dropdownmenu

---
+++ js/dropdownMenu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("dropdownMenu");
+    if (menu.style.display === "none") {
+      menu.style.display = "block";
+    } else {
+      menu.style.display = "none";
+    }
+  }
+  
