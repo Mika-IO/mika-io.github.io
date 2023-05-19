e795547d64fb87f4890e0db460fc328e43edad50
Mikaio Faria <mikaiodev@gmail.com>
2023-05-18 22:23:34 -0400

    feat: serch

---
+++ js/articles.js
@@ -0,0 +1,63 @@
+// JavaScript
+function loadArticles() {
+  // Ler o arquivo JSON (substitua "caminho/do/arquivo.json" pelo caminho correto)
+  fetch("articles/articles.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const articlesContainer = document.getElementById("articles-cards");
+      const searchInput = document.getElementById("searchInputArticles");
+
+      // Função de filtro de artigos
+      function filterArticles() {
+        const searchText = searchInput.value.toLowerCase();
+
+        // Limpar o conteúdo atual dos artigos
+        articlesContainer.innerHTML = "";
+
+        // Filtrar os artigos de acordo com o texto de pesquisa
+        const filteredArticles = data.filter(
+          (article) =>
+            article.name.toLowerCase().includes(searchText) ||
+            article.description.toLowerCase().includes(searchText)
+        );
+
+        // Iterar sobre os artigos filtrados e criar os elementos HTML correspondentes
+        filteredArticles.forEach((article) => {
+          const card = document.createElement("div");
+          card.className = "card";
+
+          const img = document.createElement("img");
+          img.src = article.img;
+
+          const title = document.createElement("h5");
+          title.textContent = article.name;
+
+          const description = document.createElement("p");
+          description.textContent = article.description;
+
+          const readMoreLink = document.createElement("a");
+          readMoreLink.href = article.url;
+          readMoreLink.textContent = "Read more";
+
+          card.appendChild(img);
+          card.appendChild(title);
+          card.appendChild(description);
+          card.appendChild(readMoreLink);
+
+          articlesContainer.appendChild(card);
+        });
+      }
+
+      // Adicionar evento de digitação ao input para filtrar os artigos
+      searchInput.addEventListener("input", filterArticles);
+
+      // Carregar todos os artigos no início
+      filterArticles();
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar os artigos:", error);
+    });
+}
+
+// Chamar a função para carregar os artigos
+loadArticles();
---
+++ js/projects.js
@@ -0,0 +1,65 @@
+function loadProjects() {
+  
+  fetch("projects/projects.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const projectsContainer = document.getElementById("projects-cards");
+      const searchInput = document.getElementById("searchInputProjects");
+
+      const filterProjects = () => {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        projectsContainer.innerHTML = "";
+
+        const filteredProjects = data.filter((project) => {
+          const projectName = project.name.toLowerCase();
+          return projectName.includes(searchTerm);
+        });
+
+        filteredProjects.forEach((project) => {
+          const card = document.createElement("div");
+          card.className = "card";
+
+          const title = document.createElement("h5");
+          title.textContent = project.name;
+
+          const description = document.createElement("p");
+          description.textContent = project.description;
+
+          const repoLink = document.createElement("a");
+          repoLink.href = project.repo_url;
+          repoLink.textContent = "Repository";
+
+          card.appendChild(title);
+          card.appendChild(description);
+          card.appendChild(repoLink);
+
+          if (project.live_url && project.live_url !== "") {
+            const liveLink = document.createElement("a");
+            liveLink.href = project.live_url;
+            liveLink.textContent = "Live";
+            card.appendChild(liveLink);
+          }
+          if (project.article_url && project.article_url !== "") {
+            const articleLink = document.createElement("a");
+            articleLink.href = project.article_url;
+            articleLink.textContent = "Article";
+            card.appendChild(articleLink);
+          }
+
+          projectsContainer.appendChild(card);
+        });
+      };
+
+      searchInput.addEventListener("input", filterProjects);
+
+      // Chamar a função de filtragem inicialmente
+      filterProjects();
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar os projetos:", error);
+    });
+}
+
+// Chamar a função para carregar os projetos
+loadProjects();
